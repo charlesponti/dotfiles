a4961c3b788a8aaa10a57637883ba7eca7a8764a
Charles Ponti <cj@ponti.io>
2020-01-31 14:25:23 -0500

    (system) add Python/3.7 to PATH

--- system/path.zsh
+++ system/path.zsh
@@ -2,7 +2,7 @@
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
 # Python
-export PYTHON_PATH="/usr/local/opt/python/libexec/bin"
+export PYTHON_PATH="/usr/local/opt/python/libexec/bin:$HOME/Library/Python/3.7/bin"
 export ARCANIST="$HOME/.arc/arcanist/bin"
 export POETRY=$HOME/.poetry/bin
 

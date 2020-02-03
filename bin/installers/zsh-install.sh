103dc9760ae17aa755c5996788a07829cdac3f85
Charles Ponti <cj@ponti.io>
2020-02-03 10:00:03 -0500

    (installers) Add Antigen & Nerd Font to zsh install

--- bin/installers/zsh-install.sh
+++ bin/installers/zsh-install.sh
@@ -4,6 +4,14 @@ brew install zsh zsh-completions zsh-autosuggestions
 # Install Oh-My-Zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
+# Install Antigen
+mkdir $HOME/.antigen
+curl -L git.io/antigen > antigen.zsh
+
+# Install Meslo font
+brew tap homebrew/cask-fonts
+brew cask install font-meslo-nerd-font
+
 # Install Powerline Fonts
 git clone https://github.com/powerline/fonts.git --depth=1;
 cd fonts;

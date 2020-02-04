4a80a6fcb2dabc559e7439b767e010de06920c6c
Charlie <cj@ponti.io>
2020-02-03 21:16:26 -0500

    (commands) Use German for afternoon message

--- commands/welcome_message.py
+++ commands/welcome_message.py
@@ -2,9 +2,18 @@ from datetime import datetime
 
 now = datetime.now()
 
+FRENCH = {
+    "morning": "Bonjour, Monsieur",
+    "evening": "Bonsoir, Monsieur"
+}
+
+GERMAN = {
+    "afternoon": "Guten Tag, Herr Ponti"
+}
+
 if now.hour < 12:
-  print("Bonjour, Monsieur")
+    print(FRENCH["morning"])
 elif now.hour > 12 and now.hour < 17:
-  print("Bonjour, Monsieur")
+    print(GERMAN["afternoon"])
 else:
-  print("Bonsoir, Monsieur")
+    print(FRENCH["evening"])

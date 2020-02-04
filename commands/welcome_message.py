aa5f256fd2b2aa61fd3ba6132b1e4254a150e54d
Charlie <cj@ponti.io>
2020-02-03 20:44:56 -0500

    (commands) fix welcome_message

--- commands/welcome_message.py
+++ commands/welcome_message.py
@@ -4,7 +4,7 @@ now = datetime.now()
 
 if now.hour < 12:
   print("Bonjour, Monsieur")
-elif now.how > 12 and now.hour < 17:
+elif now.hour > 12 and now.hour < 17:
   print("Bonjour, Monsieur")
 else:
   print("Bonsoir, Monsieur")

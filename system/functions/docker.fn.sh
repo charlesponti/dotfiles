a20cdedbdc8202434d550571fbe4823c92d1664d
Charles Ponti <cj@ponti.io>
2020-03-06 12:02:09 -0500

    (docker): Allow for setting postgres vars

--- system/functions/docker.fn.sh
+++ system/functions/docker.fn.sh
@@ -9,7 +9,7 @@ DOCKER_VOLUMES=$HOME/.docker/volumes
 # Returns:
 #   None
 #######################################
-docker-container () {
+docker-container() {
   if [ "$1" == "airflow" ]; then
     docker run \
       -d \
@@ -17,10 +17,13 @@ docker-container () {
       puckel/docker-airflow webserver
   elif [ "$1" == "postgres" ]; then
     docker run --rm \
-      --name pg-docker \
-      -e POSTGRES_PASSWORD=docker -d \
+      --name postgres \
       -p 5432:5432 \
       -v $DOCKER_VOLUMES/postgres:/var/lib/postgresql/data \
+      -e POSTGRES_USER="$2" \
+      -e POSTGRES_PASSWORD="$3" \
+      -e POSTGRES_DB="$4" \
+      -d \
       postgres
   elif [ "$1" == "rabbitmq" ]; then
     docker-compose -f $HOME/.dotfiles/services/docker-compose.yml up rabbitmq
@@ -32,7 +35,7 @@ alias dcup="docker-compose up --build --remove-orphans -d"
 alias dcdown="docker-compose down --rmi all --remove-orphans -v"
 
 # Destroy all the docker things and create that which u've destroyed!
-docker-rebuild () {
+docker-rebuild() {
   informer "🗑 Destroying all the Docker things...."
   docker-compose down --rmi all --remove-orphans -v
 

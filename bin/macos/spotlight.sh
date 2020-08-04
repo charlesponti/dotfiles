5a089255326e2b51939afdfc8bf0fb8d0f9e9543
Charles Ponti <cj@ponti.io>
2020-08-04 18:50:13 -0400

    fix: remove outdate macos settings

--- bin/macos/spotlight.sh
+++ bin/macos/spotlight.sh
@@ -7,7 +7,7 @@
 # Disable Spotlight indexing for any volume that gets mounted and has not yet
 # been indexed before.
 # Use `sudo mdutil -i off "/Volumes/foo"` to stop indexing any volume.
-sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Volumes"
+# sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Volumes"
 # Change indexing order and disable some search results
 # Yosemite-specific search results (remove them if you are using macOS 10.9 or older):
 # 	MENU_DEFINITION

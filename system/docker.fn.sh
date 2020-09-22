8b7fea5f3775a1de03e82a1ef7a361a3413f8c4d
Charles Ponti <cj@ponti.io>
2020-09-22 18:00:36 -0400

    wip

--- system/docker.fn.sh
+++ system/docker.fn.sh
@@ -1,34 +1,4 @@
-DOCKER_VOLUMES=$HOME/.docker/volumes
-
-#######################################
-# Run standard docker containers
-# Globals:
-#   HOME
-# Arguments:
-#   $1 - Container to run
-# Returns:
-#   None
-#######################################
-docker-container() {
-  if [ "$1" == "airflow" ]; then
-    docker run \
-      -d \
-      -p 8081:8080 \
-      puckel/docker-airflow webserver
-  elif [ "$1" == "postgres" ]; then
-    docker run --rm \
-      --name postgres \
-      -p 5432:5432 \
-      -v $DOCKER_VOLUMES/postgres:/var/lib/postgresql/data \
-      -e POSTGRES_USER="$2" \
-      -e POSTGRES_PASSWORD="$3" \
-      -e POSTGRES_DB="$4" \
-      -d \
-      postgres
-  elif [ "$1" == "rabbitmq" ]; then
-    docker-compose -f $HOME/.dotfiles/services/docker-compose.yml up rabbitmq
-  fi
-}
+export DOCKER_VOLUMES=$HOME/.docker/volumes
 
 alias dcup="docker-compose up --build --remove-orphans -d"
 

ff77558c3505f5ec1f9a4e4da089cbf5b2c8c4f8
Charles Ponti <cj@ponti.io>
2021-06-11 00:37:39 -0700

    remove docker-rebuild func

--- system/docker.sh
+++ system/docker.sh
@@ -5,12 +5,3 @@ alias dcup="docker-compose up --build --remove-orphans"
 alias dcdown="docker-compose down --rmi all --remove-orphans -v"
 
 alias docker-clean="docker system prune --all --force --volumes"
-
-# Destroy all the docker things and create that which u've destroyed!
-docker-rebuild() {
-  informer "🗑 Destroying all the Docker things...."
-  docker-compose down --rmi all --remove-orphans -v
-
-  informer "🏗 Rebuidling all the Docker things...."
-  docker-compose up --build
-}

8e15dbbf7992db061a1dba24d04e0f8d554a2c19
charlesponti <cj@ponti.io>
2021-06-20 17:22:02 -0700

    update python install to 3.9.5

--- bin/installers/python-install.sh
+++ bin/installers/python-install.sh
@@ -5,9 +5,12 @@
 brew install pyenv
 
 # Install Python v3.6.9
-~/.pyenv/bin/pyenv install 3.6.9
+~/.pyenv/bin/pyenv install 3.9.5
 
 # Set 3.6.9 as global version to avoid usage of system Python
-~/.pyenv/bin/pyenv global 3.6.9
+~/.pyenv/bin/pyenv global 3.9.5
+
+# Install Pygments for `pcat` alias
+pip3 install pygments
 
 exec $SHELL

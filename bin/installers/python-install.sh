2fd08863d17f7aa99b24a243cb4b141b3484c8ff
charlesponti <cj@ponti.io>
2022-10-23 20:10:02 -0700

    feat(python): set install version to 3.7.4

--- bin/installers/python-install.sh
+++ bin/installers/python-install.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-
 # Install Pyenv for managing Python versions
 brew install pyenv
 
-# Install Python v3.6.9
-~/.pyenv/bin/pyenv install 3.9.5
+# Install Python
+~/.pyenv/bin/pyenv install 3.7.4
 
-# Set 3.6.9 as global version to avoid usage of system Python
-~/.pyenv/bin/pyenv global 3.9.5
+# Set global version
+~/.pyenv/bin/pyenv global 3.7.4
 
 # Install Pygments for `pcat` alias
-pip3 install pygments
+~/.pyenv/shims/pip3 install pygments
 
 exec $SHELL

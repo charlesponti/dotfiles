763c5aaee738abcc91189c9d5d214c1e783c5aa2
charlesponti <cj@ponti.io>
2023-07-06 16:15:25 -0700

    fix(git): reorder git aliases

--- system/git.sh
+++ system/git.sh
@@ -5,80 +5,33 @@
 #-----------------------
 
 alias gb='git branch'
+alias gbd='git branch -D'
+alias gco='git checkout'
+alias gcl='git clone'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gcam='git commit -am'
 alias gcanv='git commit -m $1 --no-verify'
 # Add uncommitted and unstaged changes to the last commit
 alias gcaa="git commit -a --amend -C HEAD"
-
-alias gcl='git clone'
-alias gco='git checkout'
-alias gcount='git shortlog -sn'
-alias gbd='git branch -D'
-alias gexport='git archive --format zip --output'
+# Show commits since last pull
+alias gnew="git log HEAD@{1}..HEAD@{0}"
 alias gg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gl='git pull'
 alias gll='git log --graph --pretty=oneline --abbrev-commit'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gm="git merge"
-
-# Fetch and merge upstream
-alias gmu='git fetch origin -v; git fetch upstream -v; git merge upstream/master'
-
-# From http://blogs.atlassian.com/2014/10/advanced-git-aliases/
-# Show commits since last pull
-alias gnew="git log HEAD@{1}..HEAD@{0}"
-
-alias gp="git push"
 alias gpl='git pull --all --prune'
+alias gp="git push"
 alias gpr='git prune'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gsl="git shortlog -sn"
+alias gcount='git shortlog -sn'
 alias gus="git reset HEAD"
+alias gtls="git tag -l | gsort -V"
+alias gd='git diff | mate'
+alias gde="git diff | $EDITOR"
+alias gexport='git archive --format zip --output'
 
-case $OSTYPE in
-  darwin*)
-    alias gtls="git tag -l | gsort -V"
-    ;;
-  *)
-    alias gtls='git tag -l | sort -V'
-    ;;
-esac
-
-if [ -z "$EDITOR" ]; then
-  case $OSTYPE in
-    linux*)
-      alias gd='git diff | vim -R -'
-      ;;
-    darwin*)
-      alias gd='git diff | mate'
-      ;;
-    *)
-      alias gd='git diff'
-      ;;
-  esac
-else
-  # Remove `+` and `-` from start of diff lines; just rely upon color.
-  # alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
-  alias gd="git diff | $EDITOR"
-fi
-
-#######################################
-# Initialize a Git repository
-# Globals:
-#   HOME - Home directory
-# Arguments:
-#   None
-# Returns:
-#   None
-#######################################
-git-init() {
-  # Initialize repository
-  git init
-  # Copy .gitignore
-  cp $HOME/.dotfiles/home/.gitignore_global .gitignore
-}
 
 git-merge-main() {
   # Merge lastest main branch into current branch
@@ -96,9 +49,9 @@ gdiff() {
 git-rename-author() {
   git filter-branch --env-filter "if [ '$GIT_AUTHOR_NAME' = $1 ]; then
      GIT_AUTHOR_EMAIL=$2;
-     GIT_AUTHOR_NAME=$1;
+     GIT_AUTHOR_NAME=$3;
      GIT_COMMITTER_EMAIL=$2;
-     GIT_COMMITTER_NAME=$1; fi" -f -- --all
+     GIT_COMMITTER_NAME=$3; fi" -f -- --all
 }
 
 # Function to add, commit, and push changes if the current branch is not "main" or "beta"

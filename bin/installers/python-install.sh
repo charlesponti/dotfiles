2d85726cd9ffc1ef12bce042889e37b5eaadf28c
Charles Ponti <cj@ponti.io>
2025-02-22 14:31:40 -0800

    feat: Replace Pyenv installation with UV for Python version management

--- bin/installers/python-install.sh
+++ bin/installers/python-install.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 
-# Install Pyenv for managing Python versions
-brew install pyenv
+# Install UV for Python version and project management
+brew install uv
 
-# Install Python
-~/.pyenv/bin/pyenv install 3.9.15
-
-# Set global version
-~/.pyenv/bin/pyenv global 3.9.15
-
-# Install Pygments for `pcat` alias
-~/.pyenv/shims/pip3 install pygments
-
-exec $SHELL
+success 'Installed UV'
